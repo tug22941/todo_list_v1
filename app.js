fa400ffabefb7648ac1a7daae67948d87d288183
Jonah Saywonson <tug22941@temple.edu>
2025-10-30 19:06:12 -0400

    completed inital build

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-console.log("app.js online!")
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ""){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        
+        let pencilMP3 = new Audio('/mp3/pencil.mp3');
+        pencilMP3.play();
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+inputBox.addEventListener("keydown", function(e){
+    if(e.key=="Enter"){
+        e.preventDefault();
+        addTask();
+    }
+})
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
